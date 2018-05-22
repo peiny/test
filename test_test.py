5ef414afc16eead652473d1e3a3871b5c451598b
pei ningyuan <pz763895@dal.ca>
2018-05-22 16:50:42 -0300

    add test methods

--- test_test.py
+++ test_test.py
@@ -3,7 +3,10 @@ import awesome
 
 
 class TestMethods:
-    def test_add(self):
+    def test_smile(self):
         assert awesome.smile() == ":)"
+    
+    def test_extra(self):
+        assert awesome.addmore() ==":"
 
 
