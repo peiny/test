177fd3f52b8ae2800d597d843a5f999bac0cbce0
pei ningyuan <pz763895@dal.ca>
2018-05-22 16:44:45 -0300

    modify config

--- test_test.py
+++ test_test.py
@@ -4,9 +4,6 @@ import awesome
 
 class TestMethods:
     def test_add(self):
-        # self.assertEqual(awesome.smile(), ":)")
         assert awesome.smile() == ":)"
 
 
-# if __name__ == '__main__':
-#     unittest.main()
